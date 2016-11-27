0f6cb9ef28aa1efb088d0381608a65359302c04e
jason <jbolanos@growthaccelerationpartners.com>
2016-11-27 13:21:45 -0600

    Add metrics

--- Core.java
+++ Core.java
@@ -1,14 +1,11 @@
-
 import amqp.AMQPConsumer;
 import java.util.function.Function;
 import bigram.BigramCounter;
-
 public class Core {
     
     private AMQPConsumer consumidor;
     private BigramCounter contador;
     private boolean colaTerminada;
-
     public void run(){
         this.contador = new BigramCounter();
         this.consumidor= new AMQPConsumer();
@@ -23,10 +20,12 @@ public class Core {
             boolean finCola = bigram.equals("######END######");
             if(finCola && !colaTerminada){
                this.colaTerminada = true;
+               System.out.println("la cola original esta vacia");
             } 
             if(!(colaTerminada && consumidor.queueSize("AMQPBigram") == 0)){
               contador.processBigram(bigram);
             } else {
+              System.out.println("Guardando cambios y cerrando conexion");
               contador.storeFrequencies();
               consumidor.closeConnection();
             }
